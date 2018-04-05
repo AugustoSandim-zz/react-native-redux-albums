a12f5b689931fb16db297df656da06d102b83662
Augusto Sandim <augusto@jera.com.br>
2018-04-05 02:33:00 -0300

    Adding axios for request API

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import axios from 'axios';
 
 class AlbumList extends Component {
+  state = { albums: [] };
+
+  componentWillMount() {
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+      .then(response => this.setState({ albums: response.data }));
+  }
+
   render() {
+
     return (
       <View>
         <Text>Album List !!!!</Text>
