3382422d6e63f0b17a74f1e3104a7f91f98f1b42
Augusto Sandim <augusto@jera.com.br>
2018-04-05 00:38:38 -0300

    Adding editorconfig

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
 // Import a library to help create a component
 import React from 'react';
-import {
-  Text,
-  AppRegistry
-} from 'react-native';
+import { AppRegistry } from 'react-native';
 import Header from './src/components/header';
 
 // Create a component
-const App = () => ( 
-  <Header />
-);
-
+const App = () => <Header />;
 
 // Render it to the device
 AppRegistry.registerComponent('albums', () => App);
