85d32377a5b969f87eaf687ba42df931d8bcc885
Augusto Sandim <augusto@jera.com.br>
2018-05-04 01:32:52 -0300

    Add Abum details and Card

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,25 +1,23 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import axios from 'axios';
+import AlbumDetail from './AlbumDetail';
 
 class AlbumList extends Component {
   state = { albums: [] };
 
   componentWillMount() {
-    axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+    axios
+      .get('https://rallycoding.herokuapp.com/api/music_albums')
       .then(response => this.setState({ albums: response.data }));
   }
 
   renderAlbums() {
-    return this.state.albums.map(album => <Text key={album.title}>{album.title}</Text>);
+    return this.state.albums.map(album => <AlbumDetail key={album.title} album={album} />);
   }
 
   render() {
-    return (
-      <View>
-        { this.renderAlbums() }
-      </View>
-    );
+    return <View>{this.renderAlbums()}</View>;
   }
 }
 
